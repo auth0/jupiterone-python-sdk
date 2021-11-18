7f656cc2abae13dedd45fa677bb0043e8c0a3d3e
Tyler Walch <tywalch@gmail.com>
2021-11-18 14:07:45 -0500

    calling the J1 sync results API

--- jupiterone/constants.py
+++ jupiterone/constants.py
@@ -6,6 +6,24 @@ QUERY_V1 = """
     queryV1(query: $query, variables: $variables, dryRun: $dryRun, includeDeleted: $includeDeleted) {
       type
       data
+      url
+    }
+  }
+"""
+
+CURSOR_QUERY_V1 = """
+  query J1QL_v2($query: String!, $variables: JSON, $flags: QueryV1Flags, $includeDeleted: Boolean, $cursor: String) {
+    queryV1(
+      query: $query
+      variables: $variables
+      flags: $flags
+      includeDeleted: $includeDeleted
+      cursor: $cursor
+    ) {
+      type
+      data
+      cursor
+      __typename
     }
   }
 """
